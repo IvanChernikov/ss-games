8f830a7f5c7f7a25787d430fe5c3352ee14654a9
IvanChernikov <ivan.chernikov25@gmail.com>
2018-08-13 10:11:09 -0400

    Added Space Invaders

--- install.php
+++ install.php
@@ -20,6 +20,15 @@ $required = [
 		'twbs/bootstrap/dist/css/bootstrap.min.css',
 		'components/jqueryui/themes/smoothness/jquery-ui.min.css',
 		'components/jqueryui/themes/smoothness/theme.css',
+		'components/font-awesome/css/fontawesome-all.min.css'
+	]
+];
+
+$copy = [
+	'webfonts' => [
+		'components/font-awesome/webfonts/fa-brands-400.woff2',
+		'components/font-awesome/webfonts/fa-regular-400.woff2',
+		'components/font-awesome/webfonts/fa-solid-900.woff2'
 	]
 ];
 
@@ -27,4 +36,11 @@ foreach ($required as $type => $requirements) {
 	$files = array_map('load', $requirements);
 	$data = implode('', $files);
 	save($type, $data);
+}
+
+foreach ($copy as $folder => $files) {
+	foreach ($files as $file) {
+		$path = sprintf('%s%s%s', $folder, DIRECTORY_SEPARATOR, basename($file));
+		file_put_contents($path, load($file));
+	}
 }
